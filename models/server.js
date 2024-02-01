c639743a553e682112c4ad3d384dc6c045db4183
Miguel Angel Ramos <return.infox@gmail.com>
2024-02-01 14:40:45 -0300

    clase server

---
+++ models/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server: SocketIOServer } = require('socket.io');
+
+class Server {
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.server = http.createServer(this.app);
+    this.io = new SocketIOServer(this.server);
+
+    // Middlewares
+    this.middlewares();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+    
+    this.app.use(express.json());
+
+    // Directorio Publico
+    this.app.use(express.static('public'));
+
+  }
+}
